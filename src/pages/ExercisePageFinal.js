084e1593b57232853fc41f348d4864301c0a9a26
yusuf Demir <yusuf.ferrum@gmail.com>
2022-01-03 23:05:06 +0100

    final page css going on

--- src/pages/ExercisePageFinal.js
+++ src/pages/ExercisePageFinal.js
@@ -2,16 +2,18 @@ import React, { useState, useEffect } from 'react'
 import { collection, onSnapshot } from 'firebase/firestore';
 import db from '../firebase/firebase';
 import { useLocation } from 'react-router-dom'
-import { Col, ListGroup, ListGroupItem,Row } from 'reactstrap';
-
+import { Col, ListGroup, ListGroupItem, Row } from 'reactstrap';
+import Navi from '../components/Navi'
+import '../styles/ExercisePageFinal.css'
+import $ from 'jquery'
 
 function ExercisePageFinal() {
     const { state } = useLocation()
     const [results, setResults] = useState([])
-    const [score,setScore]=useState(0)
-    const [analysis, setAnalysis]=useState(false)
+    const [score, setScore] = useState(0)
+    const [analysis, setAnalysis] = useState(false)
+    const [situation,setSituation] = useState(false)
 
-    
     useEffect(() => {
         onSnapshot(collection(db, 'exercises-data-' + state.newTopicName), snapshot =>
             setResults(
@@ -22,76 +24,83 @@ function ExercisePageFinal() {
             )
 
         )
-        
-        
+
+
     }, [])
-    
-    const calculateScore=()=>{
-        
-        let newScore=0
-        for(let i=0;i<results.length;i++){         
-            if(state.userAnswers[i]==results[i].data.rightOption){
-                newScore=newScore+1
+
+    const calculateScore = () => {
+
+        let newScore = 0
+        for (let i = 0; i < results.length; i++) {
+            if (state.userAnswers[i] == results[i].data.rightOption) {
+                newScore = newScore + 1
             }
         }
         setScore(newScore)
     }
-    const showAnalysis = ()=>{
+    const showAnalysis = () => {
         setAnalysis(true)
+        
     }
- 
+
     return (
         <div>
-            <h1>finalPage</h1>
-            <button onClick={calculateScore}>Calculate Score</button>
-             {score}
-             <button onClick={showAnalysis}>Analysis</button>
-             {analysis===true? 
-            <Row>
-            
-                <Col xs='12'>
-                    <h1>Correct Answers</h1>
-                <ListGroup>
-            {
-                results.map(({ id, data },index) => {
-                    return(
-                   
-                        
-                            <ListGroupItem key={id}>
-                                <div>{data.question}</div>
-                                <div>
-                                    <Row>
-                                    <Col xs='6'>
-                                    <div>
-                                        <div>Your Answer</div>
-                                        <div>{state.userAnswers[index]}</div>
-                                    </div>
-                                    </Col>
-                                    <Col xs='6'>
-                                    <div>
-                                        <div>Correct Answer</div>
-                                        <div>{data.rightOption}</div>
-                                    </div>
-                                    </Col>
-                                    </Row>                             
-                                </div>                               
-                            </ListGroupItem> 
-                            
-
-                                           
-                )})
+            <Navi />
+            <main className='main'>
+                <div className='scoreContainer'>
+                    <button className='calculate' onClick={calculateScore}>Calculate Score</button>
+                    <div className='score'>{score}</div>
+                </div>
                
-            }
-            
-             </ListGroup>
-                </Col>
-            
-             
-            </Row>
-:null}
-            
-            
-             
+                <div className='scoreContainer'>
+                    <button className='calculate' onClick={showAnalysis}>Analysis</button>
+                </div>
+                {analysis === true ?
+                    <Row>
+                        <Col xs='12'>
+                            <div>
+                                {
+                                    results.map(({ id, data }, index) => {
+                                        return (
+                                            <div className='queContainer' key={id}>
+                                                <div className='que'>{data.question}</div>
+                                                <div>
+                                                    <Row>
+                                                        <Col xs='6'>
+                                                            <div>
+                                                            
+                                                                <span className='answerHead'>Your Answer</span>
+                                                                <div className='answer'>
+                                                                {
+                                                                    (state.userAnswers[index] === results[index].data.rightOption)?<div className='templateCorrect'></div>:<div className='templateWrong'></div>
+                                                                }
+                                                                    {state.userAnswers[index]}</div>
+                                                                
+                                                            </div>
+                                                        </Col>
+                                                        <Col xs='6'>
+                                                            <div>
+                                                                <span className='answerHead'>Correct Answer</span>
+                                                                <div className='answer'>{data.rightOption}</div>
+                                                            </div>
+                                                        </Col>
+                                                    </Row>
+                                                </div>
+                                                
+                                            </div>
+                                            
+                                        )
+                                    })
+                                }
+                            </div>
+                        </Col>
+                    </Row>
+                    : null}
+            </main>
+
+
+
+
         </div>
     )
 }
