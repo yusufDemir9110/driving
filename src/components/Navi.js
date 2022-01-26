1687856753fa6584e791b45327b4bfbbe63f1c17
yusuf Demir <yusuf.ferrum@gmail.com>
2022-01-26 23:40:58 +0100

    css colors

--- src/components/Navi.js
+++ src/components/Navi.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import {
   Navbar,
   NavbarBrand,
@@ -10,120 +10,81 @@ import {
   UncontrolledDropdown,
   DropdownToggle,
   DropdownMenu,
-  DropdownItem
-} from 'reactstrap'
-import { Link } from 'react-router-dom'
-import '../styles/Navi.css'
-import $ from 'jquery';
-import { collection, onSnapshot } from 'firebase/firestore';
+  DropdownItem,
+} from "reactstrap";
+import { Link } from "react-router-dom";
+import "../styles/Navi.css";
+import { collection, onSnapshot } from "firebase/firestore";
 
-import db from '../firebase/firebase';
-
-
-
-
-
-
-
-
+import db from "../firebase/firebase";
 
 function Navi() {
-  const [topics, setTopics] = useState([])
-  const [exercises, setExercises] = useState([])
+  const [topics, setTopics] = useState([]);
+  const [exercises, setExercises] = useState([]);
   useEffect(() => {
-    onSnapshot(collection(db, 'topics-data'), snapshot =>
+    onSnapshot(collection(db, "topics-data"), (snapshot) =>
       setTopics(
-        snapshot.docs.map(doc => ({
+        snapshot.docs.map((doc) => ({
           id: doc.id,
-          data: doc.data()
+          data: doc.data(),
         }))
       )
-    )
-    onSnapshot(collection(db, 'exercises-data'), snapshot =>
+    );
+    onSnapshot(collection(db, "exercises-data"), (snapshot) =>
       setExercises(
-        snapshot.docs.map(doc => ({
+        snapshot.docs.map((doc) => ({
           id: doc.id,
-          data: doc.data()
+          data: doc.data(),
         }))
       )
-    )
-    $("#li2").mouseenter(function () {
-      $("#li2").animate({ paddingRight: "100px" }, 50);
-      $(".li2").animate({ left: "80px" }, 250);
-      $(".li2").animate({ top: "-22px", paddingBottom: "0" }, 250).delay(250);
-    });
-    $("#li2").mouseleave(function () {
-      $("#li2").animate({ paddingRight: "15px" }, 50);
-      $(".li2").animate({ left: "0px", top: "0px", paddingBottom: "15px" }, 50);
-    });
-    $("#li3").mouseenter(function () {
-      $("#li3").animate({ paddingRight: "100px" }, 50);
-      $(".li3").animate({ left: "80px" }, 250);
-      $(".li3").animate({ top: "-22px", paddingBottom: "0" }, 250).delay(250);
-    });
-    $("#li3").mouseleave(function () {
-      $("#li3").animate({ paddingRight: "20px" }, 50);
-      $(".li3").animate({ left: "0px", top: "0px", paddingBottom: "15px" }, 50);
-    });
-    $("#li4").mouseenter(function () {
-      $("#li4").animate({ paddingRight: "100px" }, 50);
-      $(".li4").animate({ left: "80px" }, 250);
-      $(".li4").animate({ top: "-22px", paddingBottom: "0" }, 250).delay(250);
-    });
-    $("#li4").mouseleave(function () {
-      $("#li4").animate({ paddingRight: "20px" }, 50);
-      $(".li4").animate({ left: "0px", top: "0px", paddingBottom: "15px" }, 50);
-    });
-
-  }, [])
-
+    );
+  }, []);
 
   return (
-    <nav>
-      <div id="line">
-        <div id="logo" className="d-inline">
-          <p>LOGO</p>
-        </div>
-        <div className="menuItemList d-inline">
-          <ul className="ul1">
-            <li className="li1" id="li2">Topics
-              <ul className="ul2">
-                {
-                  topics.map(({ id, data }) => (
-                    <Link to={'/lp'} state={{ state: data.name }}>
-                      <li key={id} className='li2'>{data.name}</li>
-                    </Link>
-                  ))
-                }
-              </ul>
-            </li>
-            <li className="li1" id="li3">Exercises
-              <ul className="ul2">
-                {
-                  exercises.map(({ id, data }) => (
-                    <Link to={'/ep'} state={{ state: data.name }}>
-                      <li key={id} className='li3'>{data.name}</li>
-                    </Link>
-                  ))
-                }
-              </ul>
-            </li>
-            <li className="li1" id="li4">Language
-              <ul className="ul2">
-                <Link to='/'><li className="li4" >English</li></Link>
-                <Link to='/Du'><li className="li4" >Dutch</li></Link>
-                <Link to='/Tr'><li className="li4" >Turkish</li></Link>
-              </ul>
-            </li>
-          </ul>
-        </div>
-
+    <nav className="rowSpaceBetween">
+      <div class="logo rowCenter">
+        <div></div>
       </div>
-
+      <div class="menuMainItems">
+        <ul class="mainUl">
+          <li class="menuSubItems">
+            Topics
+            <ul class="subUl">
+              {topics.map(({ id, data }) => (
+                <Link to={"/lp"} state={{ state: data.name }}>
+                  <li key={id}>{data.name}</li>
+                </Link>
+              ))}
+            </ul>
+          </li>
+          <li class="menuSubItems">
+            Exercises
+            <ul class="subUl">
+              {exercises.map(({ id, data }) => (
+                <Link to={"/ep"} state={{ state: data.name }}>
+                  <li key={id}>{data.name}</li>
+                </Link>
+              ))}
+            </ul>
+          </li>
+          <li class="menuSubItems">
+            Languages
+            <ul class="subUl">
+              <Link to="/">
+                <li>English</li>
+              </Link>
+              <Link to="/Du">
+                <li>Dutch</li>
+              </Link>
+              <Link to="/Tr">
+                <li>Turkish</li>
+              </Link>
+            </ul>
+          </li>
+        </ul>
+      </div>
     </nav>
-  )
+  );
 }
-
-
 
 export default Navi;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,131 +1,137 @@
-import { collection, onSnapshot } from 'firebase/firestore';
-import React, { useState, useEffect } from 'react'
-import db from '../firebase/firebase';
-import { Link } from 'react-router-dom'
-import '../styles/HomePage.css'
-import { Col, Row } from 'reactstrap'
-import Navi from '../components/Navi'
-import Footer from '../components/Footer';
-import Carousel from 'react-material-ui-carousel'
-import { Paper, Button } from '@mui/material'
+import { collection, onSnapshot } from "firebase/firestore";
+import React, { useState, useEffect } from "react";
+import db from "../firebase/firebase";
+import { Link } from "react-router-dom";
+import "../styles/HomePage.css";
+import { Col, Row } from "reactstrap";
+import Navi from "../components/Navi";
+import Footer from "../components/Footer";
+import Carousel from "react-material-ui-carousel";
+import { Paper, Button } from "@mui/material";
 
 function Home() {
-    const [slides, setSlides] = useState([])
-    const [topics, setTopics] = useState([])
-    const [exercises, setExercises] = useState([])
-    useEffect(() => {
-        onSnapshot(collection(db, 'Carslider-data'), snapshot =>
-            setSlides(
-                snapshot.docs.map(doc => ({
-                    id: doc.id,
-                    data: doc.data()
-                }))
-            )
-        )
-        onSnapshot(collection(db, 'topics-data'), snapshot =>
-            setTopics(
-                snapshot.docs.map(doc => ({
-                    id: doc.id,
-                    data: doc.data()
-                }))
-            )
-        )
-        onSnapshot(collection(db, 'exercises-data'), snapshot =>
-            setExercises(
-                snapshot.docs.map(doc => ({
-                    id: doc.id,
-                    data: doc.data()
-                }))
-            )
-        )
+  const [slides, setSlides] = useState([]);
+  const [topics, setTopics] = useState([]);
+  const [exercises, setExercises] = useState([]);
+  useEffect(() => {
+    onSnapshot(collection(db, "Carslider-data"), (snapshot) =>
+      setSlides(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          data: doc.data(),
+        }))
+      )
+    );
+    onSnapshot(collection(db, "topics-data"), (snapshot) =>
+      setTopics(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          data: doc.data(),
+        }))
+      )
+    );
+    onSnapshot(collection(db, "exercises-data"), (snapshot) =>
+      setExercises(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          data: doc.data(),
+        }))
+      )
+    );
+  }, []);
 
-    }, [])
-
-    return (
-        <div>
-            <Navi />
-            <Carousel duration={1000} interval={5000} indicators={false} stopAutoPlayOnHover={false}>
-                {
-                    slides.map(({ data, id }) => (
-                        <Paper key={id} className='paper' style={{ backgroundImage: 'url(' + data.image + ')' }}>
-                            <h2 className='altText'>{data.altText}</h2>
-                            <h4 className='caption'>{data.caption}</h4>
-
-                           
-                        </Paper>
-                    ))
-                }
-            </Carousel>
-            <h1>HomeEnglish</h1>
-            <h1>Lessons</h1>
-            <div className='topic_section'>
-                <div className='topicCont'>
-                    {
-                        topics.map(({ id, data }) => (
-                            <Col style={{padding:0}} xs='12' sm='4' md='3' lg='2'>
-                                <Link to={'/lp'} style={{padding:0}} state={{ state: data.name }}>
-
-                                    <div key={id} className='topic' style={{ backgroundImage: 'url(' + data.image + ')' }}>
-
-                                        <div className='topic_name'>{data.name}</div>
-                                        <div className='topic_desc_bg'>
-                                            <div className='topic_desc' id='topic_desc_1'>{data.description1}</div>
-                                            <div className='topic_desc' id='topic_desc_2'>{data.description2}</div>
-                                            <div className='topic_desc' id='topic_desc_3'>{data.description3}</div>
-                                            <div className='topic_desc' id='topic_desc_4'>{data.description4}</div>
-                                        </div>
-
-
-                                    </div>
-
-
-                                </Link>
-
-                            </Col>
-                        ))
-
-                    }
-
+  return (
+    <div>
+      <Navi />
+      <Carousel
+        duration={1000}
+        interval={5000}
+        indicators={false}
+        stopAutoPlayOnHover={false}
+      >
+        {slides.map(({ data, id }) => (
+          <Paper
+            key={id}
+            className="paper"
+            style={{ backgroundImage: "url(" + data.image + ")" }}
+          >
+            <h2 className="altText">{data.altText}</h2>
+            <h4 className="caption">{data.caption}</h4>
+          </Paper>
+        ))}
+      </Carousel>
+      <h1>HomeEnglish</h1>
+      <h1>Lessons</h1>
+      <div className="topic_section">
+        <div className="topicCont">
+          {topics.map(({ id, data }) => (
+            <Col style={{ padding: 0 }} xs="12" sm="4" md="3" lg="2">
+              <Link
+                to={"/lp"}
+                style={{ padding: 0 }}
+                state={{ state: data.name }}
+              >
+                <div
+                  key={id}
+                  className="topic"
+                  style={{ backgroundImage: "url(" + data.image + ")" }}
+                >
+                  <div className="topic_name">{data.name}</div>
+                  <div className="topic_desc_bg">
+                    <div className="topic_desc" id="topic_desc_1">
+                      {data.description1}
+                    </div>
+                    <div className="topic_desc" id="topic_desc_2">
+                      {data.description2}
+                    </div>
+                    <div className="topic_desc" id="topic_desc_3">
+                      {data.description3}
+                    </div>
+                    <div className="topic_desc" id="topic_desc_4">
+                      {data.description4}
+                    </div>
+                  </div>
                 </div>
-
-            </div>
-            <h1>Exercises</h1>
-            <div className='topic_section'>
-                <div className='topicCont'>
-
-                    {
-                        exercises.map(({ id, data }) => (
-                            <Col style={{padding:0}} xs='12' sm='4' md='3' lg='2'>
-                                <Link to={'/ep'} state={{ state: data.name }}>
-
-                                    <div key={id} className='topic' style={{ backgroundImage: 'url(' + data.image + ')' }}>
-
-                                        <div className='topic_name'>{data.name}</div>
-                                        <div className='topic_desc_bg'>
-                                            <div className='topic_desc' id='topic_desc_1'>{data.description1}</div>
-                                            <div className='topic_desc' id='topic_desc_2'>{data.description2}</div>
-                                            <div className='topic_desc' id='topic_desc_3'>{data.description3}</div>
-                                            <div className='topic_desc' id='topic_desc_4'>{data.description4}</div>
-                                        </div>
-
-
-                                    </div>
-
-
-                                </Link>
-
-                            </Col>
-                        ))
-
-                    }
-
-                </div>
-
-            </div>
-            <Footer />
+              </Link>
+            </Col>
+          ))}
         </div>
-    )
+      </div>
+      <h1>Exercises</h1>
+      <div className="topic_section">
+        <div className="topicCont">
+          {exercises.map(({ id, data }) => (
+            <Col style={{ padding: 0 }} xs="12" sm="4" md="3" lg="2">
+              <Link to={"/ep"} state={{ state: data.name }}>
+                <div
+                  key={id}
+                  className="topic"
+                  style={{ backgroundImage: "url(" + data.image + ")" }}
+                >
+                  <div className="topic_name exercises_bg">{data.name}</div>
+                  <div className="topic_desc_bg exercises_bg">
+                    <div className="topic_desc" id="topic_desc_1">
+                      {data.description1}
+                    </div>
+                    <div className="topic_desc" id="topic_desc_2">
+                      {data.description2}
+                    </div>
+                    <div className="topic_desc" id="topic_desc_3">
+                      {data.description3}
+                    </div>
+                    <div className="topic_desc" id="topic_desc_4">
+                      {data.description4}
+                    </div>
+                  </div>
+                </div>
+              </Link>
+            </Col>
+          ))}
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
 }
-
 
 export default Home;
